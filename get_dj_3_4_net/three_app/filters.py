fbc7b34a9f21300682220b8a5d25cc0d291a1b43
timofey466 <93028349+timofey466@users.noreply.github.com>
2022-06-13 22:58:39 +0300

    Add files via upload

---
+++ get_dj_3_4_net/three_app/filters.py
@@ -0,0 +1,12 @@
+from django_filters import rest_framework as filters, DateFromToRangeFilter
+
+from three_app.models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+    created_at = DateFromToRangeFilter()
+
+    class Meta:
+        model = Advertisement
+        fields = ['created_at']
---
+++ get_dj_3_4_net/three_app/views.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from three_app.models import Advertisement
+from three_app.permissions import IsOwner
+from three_app.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated(), IsOwner()]
+        return []
